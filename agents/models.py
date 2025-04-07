34fce90e152f2a41e0a20e01ecb2a31ca9382a49
Kevin Hu <hxy9243@gmail.com>
2025-04-06 20:42:55 -0700

    updated model impl and db init

---
+++ agents/models.py
@@ -0,0 +1,48 @@
+from typing import Dict, List
+from datetime import datetime
+import uuid
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy import String, Integer, Text, DateTime, Boolean, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+Base = declarative_base()
+
+
+class Conversation(Base):
+    __tablename__ = "conversations"
+
+    id: Mapped[int] = mapped_column(
+        String(36), primary_key=True, default=lambda: str(uuid.uuid4())
+    )
+    name: Mapped[str] = mapped_column(Text)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    last_updated: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+
+    messages: Mapped[List["Message"]] = relationship('Message', back_populates='conversation')
+
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    content: Mapped[str] = mapped_column(Text)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    is_summary: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    conversation_id: Mapped[str] = mapped_column(String(36), ForeignKey('conversations.id'))
+
+    conversation: Mapped[Conversation] = relationship(back_populates='messages')
+
+
+def init_session():
+    engine = create_engine('sqlite:///database.db')
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+
+    return Session()
